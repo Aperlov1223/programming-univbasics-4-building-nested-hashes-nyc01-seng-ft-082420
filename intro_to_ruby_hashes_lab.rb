6a2b2be7f928e674a58087c392088323d0e4bb2c
Alice Perlov <aperlov23@gmail.com>
2020-07-31 18:40:23 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,9 +1,9 @@
 def base_hash
-  {:railroads =>  
+  {
+    :railroads =>
    {
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
-end
 end
 
 def monopoly_with_second_tier
